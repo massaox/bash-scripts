bb262dba1f20db7ac3376e026ffe80da00ee6c86
massaox <massaox@gmail.com>
2017-09-17 12:10:45 +0100

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -84,7 +84,7 @@ echo "server {
 
     location ~ \.php$ {
         try_files $uri =404;
-        fastcgi_pass unix:/var/run/raf.sock;
+        fastcgi_pass unix:/var/run/php-fpm/raf.sock;
         fastcgi_index index.php;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         include fastcgi_params;
